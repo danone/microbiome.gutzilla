14ddef3ea374f4a919c94121bc070598b3dc80ae
Julien Tap <Julien.Tap@danone.com>
2018-05-24 13:32:15 +0200

    parse TXN, SEQ, DIV table and rename script file. keep only DIV with 10000 reads

---
+++ data-raw/parse_SEQ_DIV.r
@@ -0,0 +1,40 @@
+library(readr)
+library(dplyr)
+library(reshape2)
+
+
+DWH_R_STR_SEQ = read_delim("data-raw/DB/formatted data/DWH_R_STR_SEQ.txt", delim="|")
+
+
+DWH_R_STR_SEQ %>% pull(adp_amp_val) %>% table  %>% sort
+
+
+DWH_R_STR_SEQ %>%
+  select(sub_id,seq_id,thn_val,adp_amp_val,spe_val) -> DWH_R_STR_SEQ_clean
+
+
+devtools::use_data(DWH_R_STR_SEQ_clean, overwrite = TRUE)
+
+
+### import diversity indexes
+
+spec_div =
+read_delim("data-raw/DB/formatted data/DWH_F_STR_DIV.txt", delim="|", n_max = 1000) %>% spec
+
+
+attributes(spec_div$cols$seq_id)[["class"]][1] <- "collector_character"
+attributes(spec_div$cols$or_res_val)[["class"]][1] <- "collector_character"
+
+
+DWH_F_STR_DIV = read_delim("data-raw/DB/formatted data/DWH_F_STR_DIV.txt", delim="|", col_types = spec_div)
+
+
+DWH_F_STR_DIV %>%
+  filter(nb_seq_num == 10000) %>%
+  select(seq_id,test_cod,st_res_num) -> DWH_F_STR_DIV_clean
+
+
+devtools::use_data(DWH_F_STR_DIV_clean, overwrite = TRUE)
+
+
+
