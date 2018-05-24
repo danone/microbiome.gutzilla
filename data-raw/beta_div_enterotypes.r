9541af3322ed2d575499f0238be7cabfc6cc85b7
Julien Tap <Julien.Tap@danone.com>
2018-05-25 01:36:30 +0200

    compute JSD distance based on genus rel. abundance

---
+++ data-raw/beta_div_enterotypes.r
@@ -0,0 +1,11 @@
+library(BiotypeR)
+
+DWH_F_STR_TXN_clean %>%
+  #filter(seq_id %in% c("10317.000006077","10317.000015703","10317.000015704")) %>%
+  select(-st_res_num) %>%
+  mutate(or_res_val = or_res_val %>% as.numeric) %>%
+  reshape2::dcast(kig_val+phy_val+cls_val+ord_val+fam_val+gen_val~seq_id, fill=0) %>%
+  select(-contains("val")) %>%
+  BiotypeR::dist.JSD() -> AGP_JSD
+
+devtools::use_data(AGP_JSD, overwrite = TRUE)
