f9d83fe2ea8a9d79ce7fdd2be446842c5bfef6b5
Julien Tap <Julien.Tap@danone.com>
2018-05-24 17:28:53 +0200

    parse VS table

--- data-raw/parse_DM.r
+++ data-raw/parse_DM.r
@@ -7,3 +7,12 @@ select(-contains("cmp"),-contains("arm"),-contains("dm"),-contains("val_"),
        -ageu_val, -study_id, -domain, -lod_dat, -unq_seq_001) -> DWH_R_STR_DM_clean
 
 devtools::use_data(DWH_R_STR_DM_clean, overwrite = TRUE)
+
+
+
+read_delim("data-raw/DB/formatted data/DTM_F_STR_VS.txt", delim="|") %>%
+  select(-study_id, -domain, -cat_val, -scat_val) -> DTM_F_STR_VS_clean
+
+devtools::use_data(DTM_F_STR_VS_clean, overwrite = TRUE)
+
+
