7ad6713577d07071a541a322c33344a312d3571b
Julien Tap <Julien.Tap@danone.com>
2018-05-25 16:09:11 +0200

    import country data

--- data-raw/parse_DM.r
+++ data-raw/parse_DM.r
@@ -16,3 +16,22 @@ read_delim("data-raw/DB/formatted data/DTM_F_STR_VS.txt", delim="|") %>%
 devtools::use_data(DTM_F_STR_VS_clean, overwrite = TRUE)
 
 
+spec_SC =
+read_delim("data-raw/DB/formatted data/DWH_F_STR_SC.txt", delim="|", n_max = 1000) %>% spec
+
+
+attributes(spec_SC$cols$seq_id)[["class"]][1] = "collector_character"
+
+DWH_F_STR_SC =
+  read_delim("data-raw/DB/formatted data/DWH_F_STR_SC.txt", delim="|", col_types = spec_SC)
+
+
+DWH_F_STR_SC %>%
+  filter(test_cod %in% c("BTH_CTRY","RESD_CTRY")) %>%
+  select(sub_id,seq_id, or_res_val, test_cod) %>%
+  reshape2::dcast(sub_id+seq_id~test_cod, value.var="or_res_val" ) -> DWH_F_STR_SC_country
+
+devtools::use_data(DWH_F_STR_SC_country, overwrite = TRUE)
+
+
+
