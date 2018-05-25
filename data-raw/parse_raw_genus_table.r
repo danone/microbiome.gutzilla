b7433a49b6bd5ce317b0681609c7d7657ec3b136
Julien Tap <Julien.Tap@danone.com>
2018-05-25 16:10:01 +0200

    start JSD parallel

--- data-raw/parse_raw_genus_table.r
+++ data-raw/parse_raw_genus_table.r
@@ -29,7 +29,37 @@ compared = merge(otu_table_L6_clean, DWH_F_STR_TXN_clean, by=c("seq_id","kig_val
 compared %>% pull(value) %>% summary
 
 
+### compute JSD distance using parallel
 
+dist.JSD_par =
+function (inMatrix,ind_x,ind_y, pseudocount = 10^(round(log10(min(as.matrix(inMatrix)[as.matrix(inMatrix) >
+                                                                            0])), 0) - 1), ...)
+{
+  KLD <- function(x, y) sum(x * log(x/y))
+  JSD <- function(x, y) sqrt(0.5 * KLD(x, (x + y)/2) + 0.5 *
+                               KLD(y, (x + y)/2))
+  matrixColSize <- length(colnames(inMatrix))
+  matrixRowSize <- length(rownames(inMatrix))
+  colnames <- colnames(inMatrix)
+  resultsMatrix <- matrix(0, matrixColSize, matrixColSize)
+  for (k in 1:matrixRowSize) {
+    for (j in 1:matrixColSize) {
+      if (inMatrix[k, j] == 0) {
+        inMatrix[k, j] <- pseudocount
+      }
+    }
+  }
+  for (i in 1:matrixColSize) {
+    for (j in 1:matrixColSize) {
+      resultsMatrix[i, j] <- JSD(as.vector(inMatrix[, i]),
+                                 as.vector(inMatrix[, j]))
+    }
+  }
+  rownames(resultsMatrix) <- colnames(resultsMatrix) <- colnames
+  resultsMatrix <- as.dist(resultsMatrix)
+  attr(resultsMatrix, "method") <- "dist"
+  return(resultsMatrix)
+}
 
 
 
