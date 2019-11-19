51a76c7a38f6f7b81d169a47ab17ee25ee334532
Julien Tap <Julien.Tap@danone.com>
2019-11-19 15:50:49 +0100

    check individuals country of birth

--- data-raw/beta_div_enterotypes.r
+++ data-raw/beta_div_enterotypes.r
@@ -11,5 +11,12 @@ library(BiotypeR)
 # devtools::use_data(AGP_JSD, overwrite = TRUE)
 
 
+# DWH_F_STR_TXN_clean %>%
+#   filter(seq_id %in% c("10317.000006077","10317.000015703","10317.000015704")) %>%
+#   select(-st_res_num) %>%
+#   mutate(or_res_val = or_res_val %>% as.numeric) %>%
+#   widely(dist.JSD)(seq_id, kig_val+phy_val+cls_val+ord_val+fam_val+gen_val, or_res_val)
+
+
 
 
