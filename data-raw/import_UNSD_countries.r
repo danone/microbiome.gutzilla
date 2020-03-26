27dd91931b65c7c0a879b50d2deaf2ea77af65a1
Julien Tap <Julien.Tap@danone.com>
2020-03-26 13:22:37 +0100

    import UN demographic pop data

--- data-raw/import_UNSD_countries.r
+++ data-raw/import_UNSD_countries.r
@@ -4,3 +4,16 @@ UNSD_countries = readxl::read_xlsx("data-raw/UNSD — Methodology.xlsx")
 
 
 usethis::use_data(UNSD_countries)
+
+
+UNSD_population = readxl::read_xlsx("data-raw/WPP2019_POP_F01_1_TOTAL_POPULATION_BOTH_SEXES.xlsx",
+                                    sheet = 1,skip = 16, na = "...")
+UNSD_pop_subregion =
+UNSD_population %>%
+  select(3,Type,`2020`) %>%
+  filter(Type=="Subregion") %>%
+  dplyr::rename(Subregion=1, n_pop_2020=3) %>%
+  dplyr::select(-2)
+
+
+usethis::use_data(UNSD_pop_subregion)
