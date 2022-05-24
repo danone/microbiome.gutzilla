fc87a27d5b975b0d675702ef9bbd12eba9199f8e
Julien Tap <Julien.Tap@danone.com>
2022-05-24 23:42:12 +0200

    fix imports

--- data-raw/curatedMetaG/import_curated_v3.r
+++ data-raw/curatedMetaG/import_curated_v3.r
@@ -1,4 +1,4 @@
-library(curatedMetagenomicsData)
+library(curatedMetagenomicData)
 library(dplyr)
 library(phyloseq)
 
@@ -10,14 +10,14 @@ curatedStudy <-
     select(where(~ !all(is.na(.x)))) %>%
     returnSamples("relative_abundance", counts = TRUE)
 
-	
-curatedStudy_phylo <- makePhyloseqFromTreeSummarizedExperiment(curatedStudy, abund_values = "relative_abundance")	
+
+curatedStudy_phylo <- makePhyloseqFromTreeSummarizedExperiment(curatedStudy, abund_values = "relative_abundance")
 
 OTU = otu_table(curatedStudy_phylo)
 TAX = tax_table(curatedStudy_phylo)
 
 save(OTU,TAX, sampleMetadata, file="curated_v3_otu_tax.rda")
 
-	
 
-	
+
+
